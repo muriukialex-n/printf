567813a642001258adb0f09299e249012b47bc48
muriukialex-n <muriukialex.n@gmail.com>
2023-04-22 16:47:32 -0700

    done correct

--- functions2.c
+++ functions2.c
@@ -106,8 +106,7 @@ int print_reverse(va_list types, char buffer[],
 		str = ")Null(";
 	}
 	for (i = 0; str[i]; i++)
-		;
-	for (i = i - 1; i >= 0; i--)
+		for (i = i - 1; i >= 0; i--)
 	{
 		char z = str[i];
 
